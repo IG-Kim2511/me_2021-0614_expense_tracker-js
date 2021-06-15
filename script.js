e25d95023c63ece44f03221102500fddaa1bbcaa
iikim <iikim2511@gmail.com>
2021-06-15 12:39:49 -0400

    js ::  4. put values in element

--- script.js
+++ script.js
@@ -17,10 +17,54 @@
 🌊 다음에 할거
  */
 
-// 🍀 let, const
+// 🍀 const
 
 const submit = document.querySelector('.submit');
 const expenseTable = document.querySelector('#expensetable');
+
+const type = document.querySelector('#type');
+const named = document.querySelector('#named');
+const date = document.querySelector('#date');
+const amount = document.querySelector('#amount');
+const options = document.querySelector('#options');
+
+
+// 🍀 let... value
+
+let typeValue =  [];
+type.addEventListener("change", function() {
+    let input = this.value;    
+    console.log(input); 
+    
+    typeValue.push(input);
+});
+
+let namedValue =  [];
+named.addEventListener("change", function() {
+    let input = this.value;      
+    namedValue.push(input);
+});
+
+let dateValue = [];
+date.addEventListener("change", function() {
+    let input = this.value;
+    let dateEntered = new Date(input);
+    console.log(input); //e.g. 2015-11-13
+
+    dateValue.push(input);
+});
+
+let amountValue =  [];
+amount.addEventListener("change", function() {
+    let input = this.value;      
+    amountValue.push(input);
+});
+
+let optionsValue =  [];
+options.addEventListener("change", function() {
+    let input = this.value;      
+    optionsValue.push(input);
+});
 
 
 /* 🍉js 6.
@@ -38,16 +82,42 @@ submit.addEventListener('click',expense)
 
 // 🍀 functions 
 
+// let tdInner = [];
+
 function expense(e) {
-    e.preventDefault();
-    
-    let tr = document.createElement('tr');
-    let td = document.createElement('td');
-    td.className = "expense_td";
-    td.innerHTML = "input.value";   //
+    e.preventDefault();    
+
+    //2 value taking    
+
+    // 3 create element on table
+    let tr = document.createElement('tr');    
     expenseTable.append(tr);
+
+    let td = document.createElement('td');   
     tr.append(td);
+    td.innerHTML = `${typeValue}`;
     
+    td = document.createElement('td');     
+    tr.append(td);
+    td.innerHTML = `${namedValue}`;
+    
+    td = document.createElement('td');     
+    tr.append(td);
+    td.innerHTML = `${dateValue}`;
+    
+    td = document.createElement('td');     
+    tr.append(td);
+    td.innerHTML = `${amountValue}`;
+    
+    td = document.createElement('td');     
+    tr.append(td);
+    td.innerHTML = `${optionsValue}`;
+
+    // typeValue = [PaymentMethods];
+    // namedValue = "";
+    // dateValue = '';
+    // amountValue = '';
+    // optionsValue = '';
 }
 
   
